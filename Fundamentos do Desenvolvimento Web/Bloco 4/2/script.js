b7fe715b83b4c665415b383f457f3760643a2ede
Batista <jorge.lsbatista@gmail.com>
2022-02-03 15:53:06 -0300

    conclusão da primeira parte dos exercícios e início da segunda

--- Fundamentos do Desenvolvimento Web/Bloco 4/2/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 4/2/script.js
@@ -41,3 +41,13 @@ console.log(indexOfTask);
 // 1
 
 // For loop
+
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+console.log(cars[0]); // Saab
+console.log(cars[1]); // Volvo
+console.log(cars[2]); // BMW
+
+for (let index = 0; index < cars.length; index += 1) {
+  console.log(cars[index]);
+}
--- Fundamentos do Desenvolvimento Web/Bloco 4/2/script_exercicios.js
+++ Fundamentos do Desenvolvimento Web/Bloco 4/2/script_exercicios.js
@@ -15,3 +15,17 @@ console.log(indexOfPortfolio);
 
 console.log(menu.push('Contato'));
 console.log(menu);
+
+// Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let i = 0; i < groceryList.length; i += 1) {
+  console.log(groceryList[i]);
+}
+
+// Utilize o for/of para imprimir os elementos da lista names com o console.log() :
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let name of names) {
+  console.log(name);
+}
