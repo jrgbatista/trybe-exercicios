a174ff6c4a40ad16f7b4155fe949f5a03a86a335
Batista <jorge.lsbatista@gmail.com>
2022-02-09 15:35:02 -0300

    adiciona resolução dos exercícios sobre getElementsByClass() e getElementsByTagName() nos arquivos script.js e index.html

--- Fundamentos do Desenvolvimento Web/Bloco 5/1/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 5/1/script.js
@@ -1,3 +1,4 @@
+// exercício getElementById()
 const paragraph = document.getElementById("paragraph");
 paragraph.style.color = "red";
 
@@ -8,5 +9,16 @@ title.innerText = "Este título foi inserido a através de JavaScript";
 const secondP = document.getElementById("second-paragraph");
 secondP.innerText = "Eu não sei o que escrever neste parágrafo, mas aprender DOM é bastante interessante.";
 
-const subtitle = document.getElementById("subtitle");
-subtitle.innerText = "Um subtítulo seria uma legenda???"
+// const subtitle = document.getElementById("subtitle");
+// subtitle.innerText = "Um subtítulo seria uma legenda???";
+
+// exercício getElementsByClassName() e getElementsByTagName()
+let everyP = document.getElementsByClassName("all-p");
+for (i = 0; i < everyP.length; i += 1) {
+  everyP[i].innerText = "Todos os parágrafos devem conter o mesmo texto PORQUE SIM";
+}
+
+everyP[0].style.color = "blue";
+
+let subtitle = document.getElementsByTagName("h4");
+subtitle[0].style.color = "green";
