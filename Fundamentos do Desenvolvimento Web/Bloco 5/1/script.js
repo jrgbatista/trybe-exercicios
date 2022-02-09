05187b1565c6d4860c92fee90ec64c39c90c739e
Batista <jorge.lsbatista@gmail.com>
2022-02-09 15:05:42 -0300

    resolução do primeiro exercício sobre getElementById()

---
+++ Fundamentos do Desenvolvimento Web/Bloco 5/1/script.js
@@ -0,0 +1,12 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+const title = document.getElementById("page-title");
+title.style.color = "purple";
+title.innerText = "Este título foi inserido a através de JavaScript";
+
+const secondP = document.getElementById("second-paragraph");
+secondP.innerText = "Eu não sei o que escrever neste parágrafo, mas aprender DOM é bastante interessante.";
+
+const subtitle = document.getElementById("subtitle");
+subtitle.innerText = "Um subtítulo seria uma legenda???"
