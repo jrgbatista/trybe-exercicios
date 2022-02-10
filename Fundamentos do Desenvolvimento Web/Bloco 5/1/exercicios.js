2360c8c356ad5db7e1410ee19ef696c9610f9a68
Batista <jorge.lsbatista@gmail.com>
2022-02-09 22:16:23 -0300

    resolução dos exercícios

--- Fundamentos do Desenvolvimento Web/Bloco 5/1/exercicios.js
+++ Fundamentos do Desenvolvimento Web/Bloco 5/1/exercicios.js
@@ -10,3 +10,10 @@
  Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
  Crie uma função que exiba o conteúdo de todas as tags <p> no console.
         */
+
+document.querySelector("p").innerText = "Me vejo com 30 anos";
+document.querySelector(".center-content").style.backgroundColor = "white";
+document.querySelector(".main-content").style.backgroundColor = "rgb(76, 164, 109)";
+document.querySelector("h1").innerText = "Exercício 5.1 - JavaScript"
+document.querySelectorAll("p")[0].style.textTransform = "uppercase";
+document.querySelectorAll("p")[2].style.textTransform = "uppercase";
--- Fundamentos do Desenvolvimento Web/Bloco 5/1/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 5/1/script.js
@@ -3,7 +3,7 @@ document.querySelector("#paragraph").style.color = "red";
 document.querySelector("#page-title").style.color = "purple";
 document.querySelector("#page-title").innerText = "Este título foi inserido a através de JavaScript";
 
-document.querySelector("#second-paragraph").innerText = "Eu não sei o que escrever neste parágrafo, mas aprender DOM é bastante interessante.";;
+document.querySelector("#second-paragraph").innerText = "Eu não sei o que escrever neste parágrafo, mas aprender DOM é bastante interessante.";
 
 let everyP = document.querySelectorAll(".all-p");
 for (i = 0; i < everyP.length; i += 1) {
