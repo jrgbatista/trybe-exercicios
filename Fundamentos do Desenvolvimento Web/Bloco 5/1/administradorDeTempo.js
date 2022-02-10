88e4c8649af4a81528cbc58df46c2688b94010d9
Batista <jorge.lsbatista@gmail.com>
2022-02-09 21:06:08 -0300

    adiciona arquivos exercicios.html, .css e .js

---
+++ Fundamentos do Desenvolvimento Web/Bloco 5/1/administradorDeTempo.js
@@ -0,0 +1,6 @@
+document.querySelector(".emergency-tasks").style.backgroundColor = "pink";
+document.querySelector(".non-emergency-tasks").style.backgroundColor = "yellow";
+document.querySelector("#container").style.backgroundColor = "gray";
+document.querySelector("#header-container").style.backgroundColor = "green";
+document.querySelector("#footer-container").style.backgroundColor = "green";
+document.querySelectorAll("h3").style.backgroundColor = "purple";
