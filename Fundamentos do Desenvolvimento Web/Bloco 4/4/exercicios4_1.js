f9d11a52be5cad575e3a83e27a8d24376c661375
Batista <jorge.lsbatista@gmail.com>
2022-02-16 17:50:02 -0300

    conclusão dos exercícios 4.2

--- Fundamentos do Desenvolvimento Web/Bloco 4/4/exercicios4_1.js
+++ Fundamentos do Desenvolvimento Web/Bloco 4/4/exercicios4_1.js
@@ -1,48 +1,56 @@
-let info = [
-    {
-        personagem: 'Margarida',
-        origem: 'Pato Donald',
-        nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    }
-];
-
-info[1] =
-{
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim'
+// Objetos
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
-console.log(`Bem-vinda, ${info[0].personagem}`);
 
-info[0]['recorrente'] = 'Sim';
+console.log('Bem-vinda, ' + info.personagem);
 
-for (let key in info) {
-    console.log(key);
+info['recorrente'] = 'Sim';
+
+for (let properties in info) {
+  console.log(`${properties}: ${info[properties]}`);
 }
 
-for (let value in info) {
-    console.log(info[value]);
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+
+for (let properties in info) {
+  if (
+    properties === 'recorrente' &&
+    info[properties] === 'Sim' &&
+    info2[properties] === 'Sim'
+  ) {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[properties] + ' e ' + info2[properties]);
+  }
 }
 
 let leitor = {
-    nome: 'Julia',
-    sobrenome: 'Pessoa',
-    idade: 21,
-    livrosFavoritos: [
-        {
-            titulo: 'O Pior Dia de Todos',
-            autor: 'Daniela Kopsch',
-            editora: 'Tordesilhas',
-        },
-    ],
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
 };
 console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama "${leitor.livrosFavoritos[0].titulo}".`)
 
-leitor.livrosFavoritos[1] =
-{
+leitor.livrosFavoritos.push(
+  {
     titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-    autor: 'JK Rowling',
-    editora: 'Rocco',
-}
-console.log(`${leitor.nome} já tem ${leitor.livrosFavoritos.length} livros favoritos.`)
+    autor: 'J. K. Rowling',
+    editora: 'Rocco'
+  }
+);
+console.log(`${leitor.nome} tem ${leitor.livrosFavoritos.length} livros favoritos.`);
