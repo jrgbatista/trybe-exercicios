9cc4f378c596eda2e0826e2e3b1c4a4501e40e14
Batista <jorge.lsbatista@gmail.com>
2022-02-17 19:23:13 -0300

    adição de arquivos index.html e script.js e resolução dos exercícios de busca de elementos

---
+++ Fundamentos do Desenvolvimento Web/Bloco 5/2/script.js
@@ -0,0 +1,4 @@
+document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = 'pink';
+document.querySelector('#elementoOndeVoceEsta').firstElementChild.innerText = 'primeiro texto aleatório com o intuito de testar se isso aqui está funcionando adequadamente.'
+document.querySelector('#pai').firstElementChild.nextElementSibling;
+document.querySelector('#pai').firstElementChild.nextElementSibling.firstElementChild;
