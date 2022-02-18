9ab5755c81667161cc43a4ec862c607447007f9c
Batista <jorge.lsbatista@gmail.com>
2022-02-18 09:04:29 -0300

    fim do conteúdo e início dos exercícios

--- Fundamentos do Desenvolvimento Web/Bloco 5/2/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 5/2/script.js
@@ -1,4 +1,20 @@
-document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = 'pink';
+// buscando elementos
+document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = 'red';
 document.querySelector('#elementoOndeVoceEsta').firstElementChild.innerText = 'primeiro texto aleatório com o intuito de testar se isso aqui está funcionando adequadamente.'
 document.querySelector('#pai').firstElementChild.nextElementSibling;
 document.querySelector('#pai').firstElementChild.nextElementSibling.firstElementChild;
+
+// criando elementos
+let buscarElemento = document.querySelector('#elementoOndeVoceEsta');
+
+let novoElemento = document.createElement('h1');
+novoElemento.innerText = 'H1 criado com JavaScript';
+novoElemento.style.color = 'blue';
+
+buscarElemento.appendChild(novoElemento);
+
+// removendo elementos
+let removeSon = novoElemento;
+if (removeSon.innerText.includes('criado')) {
+  buscarElemento.removeChild(removeSon);
+}
