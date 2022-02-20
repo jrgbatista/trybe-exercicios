fe915f1417ec644f347e3d34b9e12fa5f7eebad6
Batista <jorge.lsbatista@gmail.com>
2022-02-20 20:09:26 -0300

    alteração no arquivo main.js

--- Fundamentos do Desenvolvimento Web/Bloco 5/3/main.js
+++ Fundamentos do Desenvolvimento Web/Bloco 5/3/main.js
@@ -11,23 +11,43 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+function addTechToLi(event) {
+  let techLi = document.querySelector('.tech');
+  techLi.classList.remove('tech');
+  event.target.classList.add('tech');
+  input.value = '';
+}
+
+firstLi.addEventListener('click', addTechToLi);
+secondLi.addEventListener('click', addTechToLi);
+thirdLi.addEventListener('click', addTechToLi);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+input.addEventListener('input', function (event) {
+  let techElement = document.querySelector('.tech');
+  techElement.innerText = event.target.value;
+})
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+myWebpage.addEventListener('dblclick', function () {
+  window.location.replace('https://github.com/jrgbatista')
+})
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+myWebpage.addEventListener('mouseover', function (event) {
+  event.target.style.color = 'pink';
+})
 
 // Segue abaixo um exemplo do uso de event.target:
 
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
+  event.target.innerText = 'Você clicou duas vezes';
   // O event possui várias propriedades, porém a mais usada é o event.target,
   // que retorna o objeto que disparou o evento.
 }
