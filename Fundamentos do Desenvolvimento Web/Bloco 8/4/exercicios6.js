5155cb82a8fafeb78a75ca5df66cd45849483e32
Batista <jorge.lsbatista@gmail.com>
2022-05-03 22:54:58 -0300

    conclusão do exercício 6 (sem refatoração)

--- Fundamentos do Desenvolvimento Web/Bloco 8/4/exercicios6.js
+++ Fundamentos do Desenvolvimento Web/Bloco 8/4/exercicios6.js
@@ -7,8 +7,22 @@ const expected = [
   { name: 'Maria Clara', average: 8.8 },
 ];
 
+const sumGrades = (acc, grade) => acc + grade;
+
+const calcAverage = (index) => {
+  const sum = grades[index].reduce(sumGrades);
+  return sum / grades[index].length;
+}
+
 function studentAverage() {
   // escreva seu código aqui
-  return
+  return students.reduce((acc, curr, index) => {
+    const obj = {
+      name: curr,
+      average: calcAverage(index)
+    };
+    acc.push(obj);
+    return acc;
+  }, []);
 }
 console.log(studentAverage());
