288de3a4ebec6c7e036239aecdef631acfd233ab
Batista <jorge.lsbatista@gmail.com>
2022-05-12 19:37:41 -0300

    conclusão dos exercícios de React

---
+++ FrontEnd/Bloco 10/1/exercicios/react-exercises/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Estudar'];
+
+function App() {
+  return (
+    <div>
+      {/* <ul>{tarefas.map(tarefa => Task(tarefa))}</ul> */}
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
