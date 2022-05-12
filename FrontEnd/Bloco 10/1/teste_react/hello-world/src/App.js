614a3ca198ed94b433a373c353e10aedf37e8d98
Batista <jorge.lsbatista@gmail.com>
2022-05-12 18:59:28 -0300

    conclusão dos exercícios de fixação em React

---
+++ FrontEnd/Bloco 10/1/teste_react/hello-world/src/App.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import './App.css';
+import HelloWorld from './HelloWorld';
+import NewComponent from './NewComponent';
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+      <NewComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ FrontEnd/Bloco 10/1/teste_react/hello-world/src/NewComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class NewComponent extends React.Component {
+  render() {
+    return <div><h1>Jorge Batista</h1>
+      <p>Futura pessoa dev!</p></div>
+  }
+}
+
+export default NewComponent;
