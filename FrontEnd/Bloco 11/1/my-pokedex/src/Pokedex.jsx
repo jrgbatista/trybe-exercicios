b07a14b736719dcd2c68c333409d9e2e71dcfa1f
Batista <jorge.lsbatista@gmail.com>
2022-05-17 16:35:53 -0300

    adição de arquivos de estilização para Pokedex e Pokemon

--- FrontEnd/Bloco 11/1/my-pokedex/src/Pokedex.jsx
+++ FrontEnd/Bloco 11/1/my-pokedex/src/Pokedex.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Pokemon from './Pokemon';
+import './pokedex.css';
 
 class Pokedex extends Component {
   state = {
@@ -45,21 +46,33 @@ class Pokedex extends Component {
   render() {
     const { myListPokemons, currentIndex } = this.state;
     return (
-      <section>
-        <div>
-          <button onClick={() => this.filterPokemon('')}>All</button>
-          {this.getPokemonTypes().map(pokemonType =>
-            (<button key={pokemonType} onClick={() => this.filterPokemon(pokemonType)}>{pokemonType}</button>)
-          )}
+      <section className='pokedex'>
+        <div className='pokedex-buttons-panel'>
+          <button
+            onClick={() => this.filterPokemon('')}
+            className='filter-button'
+          >All
+          </button>
+          {this
+            .getPokemonTypes().map(pokemonType =>
+            (<button
+              key={pokemonType}
+              onClick={() => this.filterPokemon(pokemonType)}
+              className='filter-button'
+            >{pokemonType}
+            </button>)
+            )}
         </div>
-        <div className='pokedex'>
+        <div>
           <Pokemon key={myListPokemons[currentIndex].id} pokemons={myListPokemons[currentIndex]} />
         </div>
         <div>
           <button
             onClick={() => this.next()}
             disabled={this.listHasLessThanTwo(myListPokemons)}
-          >Next</button>
+            className='pokedex-button'
+          >Next
+          </button>
         </div>
       </section >
     );
--- FrontEnd/Bloco 11/1/my-pokedex/src/Pokemon.jsx
+++ FrontEnd/Bloco 11/1/my-pokedex/src/Pokemon.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
+import './pokemon.css';
 
 class Pokemon extends Component {
   render() {
