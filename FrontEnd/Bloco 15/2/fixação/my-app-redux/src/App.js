1ad5fd5caa9ad464d9d3ea09288bbfb511631955
Batista <jorge.lsbatista@gmail.com>
2022-06-09 20:49:08 -0300

    fim dos exercícios de fixação

---
+++ FrontEnd/Bloco 15/2/fixação/my-app-redux/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import InputsList from './components/InputList';
+import List from './components/List';
+
+function App() {
+  return (
+    <div>
+      <InputsList />
+      <List />
+    </div>
+  );
+}
+
+export default App;
